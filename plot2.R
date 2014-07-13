501844496b03dd4b1a3d45eae57f79079078c484
Derek Franks <derekfranks@gmail.com>
2014-07-13 12:35:12 -0500

    upload

---
+++ plot2.R
@@ -0,0 +1,14 @@
+dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataset_url, "household_power_consumption.zip")
+unzip("household_power_consumption.zip", exdir="household_power_consumption")
+
+dat <- read.table("household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
+
+dat$Date <- as.Date(dat$Date, format="%d/%m/%Y")
+dat_sub <- subset(dat, dat$Date=="2007-02-01" | dat$Date=="2007-02-02")
+dat_sub$datetime <- paste(dat_sub$Date, dat_sub$Time)
+dat_sub$datetime <- strptime(dat_sub$datetime, "%Y-%m-%d %H:%M:%S")
+
+dat_sub$Global_active_power <- as.numeric(dat_sub$Global_active_power)
+
+plot(dat_sub$datetime, dat_sub$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
